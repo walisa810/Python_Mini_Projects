c632b445dcd1c0fd0ab90e5b35bb73760f6ee170
walisa810 <160372754+walisa810@users.noreply.github.com>
2024-11-16 00:42:32 +0600

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+#1.Madlibs
+
+# It would have some paragraphs and some blanks that would be filled by user input
+# implemented using string concatenation
+
+
+adj1 = input("Adjective :")
+vrb1 = input("Verb :")
+vrb2 = input("Verb :")
+famous_person = input("Famous Person :")
+
+madlib = f"Computer programming is so {adj1}! It makese so excited all the time because \
+I love to {vrb1}. Stay hydrated and {vrb2} like you are {famous_person}! "
+print(madlib)
